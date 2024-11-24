74d4a5f730e024119bba10aa9c01df3925bd5269
johan <johan.delgado@gmail.com>
2024-11-23 23:52:05 -0500

    2da subida de controller usuario

--- redesprofesionales/src/main/java/com/miempresa/redesprofesionales/controller/UsuarioController.java
+++ redesprofesionales/src/main/java/com/miempresa/redesprofesionales/controller/UsuarioController.java
@@ -1,9 +1,17 @@
 package com.miempresa.redesprofesionales.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.miempresa.redesprofesionales.model.Usuario;
 import com.miempresa.redesprofesionales.repository.UsuarioRepository;
 
 @RestController
@@ -12,3 +20,14 @@ public class UsuarioController {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @PostMapping
+    public Usuario crearUsuario(@RequestBody Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> verUsuario(@PathVariable String id) {
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        return usuario.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+}
