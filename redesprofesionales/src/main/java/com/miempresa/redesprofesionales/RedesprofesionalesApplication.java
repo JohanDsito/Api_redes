809979b8dae40de383ad9aff151c91dbe9321fb5
johan <johan.delgado@gmail.com>
2024-11-23 23:34:38 -0500

    Inicialización del proyecto

---
+++ redesprofesionales/src/main/java/com/miempresa/redesprofesionales/RedesprofesionalesApplication.java
@@ -0,0 +1,13 @@
+package com.miempresa.redesprofesionales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedesprofesionalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedesprofesionalesApplication.class, args);
+	}
+
+}
---
+++ redesprofesionales/src/test/java/com/miempresa/redesprofesionales/RedesprofesionalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miempresa.redesprofesionales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedesprofesionalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
